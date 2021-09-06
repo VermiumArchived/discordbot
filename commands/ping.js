7c29799c055e4368988940444ddfbb2b6ec5fd1e
Axel Olausson Holtenäs <vermium@zyner.org>
2021-09-06 19:50:07 +0200

    Minor change to ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,38 +1,32 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Check latency of the bot'),
-    async execute(interaction) {
-
-        const botAvatar = interaction.client.user.displayAvatarURL();
-        const latency = `${Date.now() - interaction.createdTimestamp}ms`;
-        const apiLatency = `${Math.round(interaction.client.ws.ping)}ms`;
-
-        const embed = {
-            color: 0x0099ff,
-            title: 'Ping',
-            description: 'Current ping of the bot',
-            thumbnail: {
-                url: botAvatar,
-            },
-            fields: [{
-                name: 'Latency',
-                value: latency,
-                inline: true,
-            }, {
-                name: 'API Latency',
-                value: apiLatency,
-                inline: true,
-            }, ],
-            timestamp: new Date(),
-            footer: {
-                text: 'Made by Zyner',
-                icon_url: botAvatar,
-            },
-        };
-
-        await interaction.reply({ embeds: [embed] });
-    },
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Check latency of the bot"),
+  async execute(interaction) {
+    const embed = {
+      color: 0x40887b,
+      title: "Ping",
+      description: "Current latency of the bot.",
+      fields: [
+        {
+          name: "⏱️ Latency",
+          value: `${Date.now() - interaction.createdTimestamp}ms`,
+          inline: true,
+        },
+        {
+          name: "📦 API Latency",
+          value: `${Math.round(interaction.client.ws.ping)}ms`,
+          inline: true,
+        },
+      ],
+      timestamp: new Date(),
+      footer: {
+        text: "Made by Zyner",
+        icon_url: interaction.client.user.displayAvatarURL(),
+      },
+    };
+    await interaction.reply({ embeds: [embed] });
+  },
 };
